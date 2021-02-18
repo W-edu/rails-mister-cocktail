d90aa284e72ba8c396e45c01da1e0563a828eea8
alec wayman <aejw_edu@pm.me>
2021-02-18 14:35:59 +0000

    cocktail_id and ingredient_id should have been references not integers

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,15 +1,6 @@
 class Ingredient < ApplicationRecord
   has_many :doses
-  has_many :cocktails, through: :doses
-
-  before_destroy :check_used
 
   validates :name, presence: true
   validates :name, uniqueness: true
-
-  private
-
-  def check_used
-    
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_113817) do
+ActiveRecord::Schema.define(version: 2021_02_18_142927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,11 +22,13 @@ ActiveRecord::Schema.define(version: 2021_02_18_113817) do
   end
 
   create_table "doses", force: :cascade do |t|
-    t.string "description"
-    t.integer "cocktail_id"
-    t.integer "ingredient_id"
+    t.text "description"
+    t.bigint "cocktail_id", null: false
+    t.bigint "ingredient_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -35,4 +37,6 @@ ActiveRecord::Schema.define(version: 2021_02_18_113817) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
